5bcc75fe1c98693b7455cc81c8891b4b9b43a0d7
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2023-11-25 10:20:14 -0500

    add routes (hello & compute) and PrimeNG module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
+import { PrimengModule } from './primeng/primeng.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, PrimengModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'jaxrs-ng';
+  constructor(private router: Router, private route: ActivatedRoute) {}
+
+  toHello($event: MouseEvent) {
+    return this.router.navigate(['say-hello']);
+  }
+  toCompute($event: MouseEvent) {
+    return this.router.navigate(['compute']);
+  }
+  title = 'JAX-RS with Angular';
 }
